45500af2eabbd0b701faa5fa72c822cdedfe0542
Gabriel Gonçalves <gabrielgrv2015@outlook.com>
2021-10-18 20:48:04 -0300

    Create styles.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
+import styles from './styles'
 
 export default function Home() {
     return(
-        <View>
-            <Text>Hello World</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Bookshelf</Text>
+            <TextInput style={styles.search} placeholder="What kind of book you wanna read today?"></TextInput>
         </View>
     )
 }
---
+++ src/pages/home/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: 40,
+    },
+
+    title: {
+        fontSize: 30,
+        marginBottom: 16,
+        marginTop: 48,
+        color: '#13131A',
+        fontWeight: 'bold'
+    },
+
+    search: {
+        fontSize: 16,
+        lineHeight: 24,
+        borderColor: '#828282',
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 10
+    }
+})
